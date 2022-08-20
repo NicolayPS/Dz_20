089dcab5b393c28e172ec4954aca7507264e2785
oksanaPC2 <77opod@gmail.com>
2022-08-20 15:19:50 +0300

    Добавьте файлы проекта.

---
+++ Dz_20/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz_20
+{
+    class Program
+    {
+        delegate double MyDelegate(double r);
+        static void Main(string[] args)
+        {
+            double r = 2;
+            MyDelegate myDelegate = Length;
+            double n = myDelegate(r);
+            Console.WriteLine(n);
+
+            myDelegate = Area;
+            n = myDelegate(r);
+            Console.WriteLine(n);
+
+            myDelegate = Volume;
+            n = myDelegate(r);
+            Console.WriteLine(n);
+
+            Console.ReadKey();
+            
+        }
+        static double Length(double r) => (double)2 * Math.PI * r;
+        static double Area(double r) => (double)Math.PI * Math.Pow(r, 2);
+        static double Volume(double r) => (double)4/3 * Math.PI * Math.Pow(r,3);
+    }
+}
